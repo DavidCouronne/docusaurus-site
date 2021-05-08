da3ad8ee66ea1f8a2d463752399f8a4c836e6d94
David Couronné <couronne.david@gmail.com>
2021-05-08 16:52:50 +0200

    prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,23 +43,7 @@ module.exports = {
             },
           ],
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
+
         {
           title: 'More',
           items: [
